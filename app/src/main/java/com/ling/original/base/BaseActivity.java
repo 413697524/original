55221ed67d7737d55193263a40d154c57825a2a7
Administrator <413697524@qq.com>
2017-03-14 09:52:55 +0800

    first commit

---
+++ app/src/main/java/com/ling/original/base/BaseActivity.java
@@ -0,0 +1,28 @@
+package com.ling.original.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayout());
+        initView();
+        initData();
+
+    }
+
+    protected abstract int getLayout();
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+
+
+}
