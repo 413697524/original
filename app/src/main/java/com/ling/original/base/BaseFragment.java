69eef50d60b2d192972f376483a5b46bbec9d724
Administrator <413697524@qq.com>
2017-03-14 11:57:27 +0800

    ddd

---
+++ app/src/main/java/com/ling/original/base/BaseFragment.java
@@ -0,0 +1,29 @@
+package com.ling.original.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+
+public abstract class BaseFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayout(),null);
+        initView();
+        initData();
+        return view;
+    }
+
+    protected abstract int getLayout();
+    protected abstract void initView();
+    protected abstract void initData();
+
+}
---
+++ app/src/main/java/com/ling/original/fragment/UserInfoFragment.java
@@ -0,0 +1,37 @@
+package com.ling.original.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ling.original.R;
+import com.ling.original.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/3/14.
+ */
+
+public class UserInfoFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
